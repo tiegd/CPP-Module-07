935e670cf93029552d98c7a158aea5a3dd77c114
Gautier Ducurtil <gaducurt@student.42.fr>
2026-06-19 11:47:55 +0200

    ex00

--- ex00/include/whatever.hpp
+++ ex00/include/whatever.hpp
@@ -6,7 +6,26 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 13:35:01 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/06/10 13:35:02 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/06/19 11:45:06 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+template <typename T> T max(T a, T b)
+{
+	return ((a > b) ? a : b);
+}
+
+template <typename T> T min(T a, T b)
+{
+	return ((a < b) ? a : b);
+}
+
+template <typename T> void swap(T &a, T &b)
+{
+	T	tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+	// return ();
+}

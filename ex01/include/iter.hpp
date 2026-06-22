bd6343ff5b76ee78f7ef90797b85e3aeb124507e
Gautier Ducurtil <gaducurt@student.42.fr>
2026-06-22 11:51:28 +0200

    iter

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -6,11 +6,38 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 14:40:33 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/06/19 14:41:46 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/06/22 11:46:53 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template <typename T> void iter(T a, T b, T c)
+#include <cstdlib>
+#include <iostream>
+
+template <typename T> void iter(T* array, const std::size_t len, void (*f)(T&))
 {
-    
+	for (std::size_t i = 0; i < len; i++)
+	{
+		f(array[i]);
+		std::cout << array[i];
+	}
+	std::endl;
+}
+
+template <typename T> void iter(const T* array, const std::size_t len, void (*f)(const T&))
+{
+	for (std::size_t i = 0; i < len; i++)
+	{
+		f(array[i]);
+	}
+	std::endl;
+}
+
+template <typename T> const void printArray(T element)
+{
+	std::cout << element;
+}
+
+template <typename T> void increment(T element)
+{
+	element++;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,17 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 14:40:29 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/06/19 14:40:30 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/06/22 11:49:00 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+int main()
+{
+	const std::string	str = "coucou";
+	int			intArray[5] = {0, 1, 2, 3, 4};
+	double		doubleArray[3] = {0.5, 6.1, 3.87};
+
+	iter(str, str.size(), &printArray);
+}
